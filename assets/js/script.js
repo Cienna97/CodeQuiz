63ed04653250ea8f02749e7ce6c44ae22ffbde4e
Cienna97 <cmortenson97@gmail.com>
2021-10-10 21:58:16 -0700

    adding test quiz questions and working on function of quiz

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+    
+}
+
+
+
+var questions = [
+    {
+        question: "What is my name?",
+        answers: {
+            a: 'Peanut',
+            b: 'Vicky',
+            c: 'Cienna'
+        },
+        correctAnswer: 'c'
+    },
+]
+
+
+
+
+
+
+var buttonEl = document.querySelector("#save-data")
