7052cb226d7647582d0ba1cc224a6926c688bb9f
Cienna97 <cmortenson97@gmail.com>
2021-10-10 22:24:10 -0700

    adding question functions and variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,30 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
     var output = [];
     var answers;
+
+    for(var i=0; i<questions.length; i++){
+
+        answers = [];
+
+        for(letter in questions[i].answers){
+
+            answers.push(
+                '<label>'
+                + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                + letter + ': '
+                + questions[i].answers[letter]
+            +'</label>'
+            );
+        }
+
+        output.push(
+            '<div class="questions">' + questions[i].question + '</div>'
+            + '<div class="answers">' + answers.join('') + '</div>'
+        );
+    }
+    quizContainer.innerHTML = output.join('');
 }
+
 
 function showQuestions(questions, quizContainer){
 
@@ -21,7 +44,7 @@ submitButton.onclick = function(){
 
 var questions = [
     {
-        question: "What is my name?",
+        questions: "What is my name?",
         answers: {
             a: 'Peanut',
             b: 'Vicky',
