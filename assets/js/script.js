cdc3503fcab36258081add0d9da09421718ebf53
Cienna97 <cmortenson97@gmail.com>
2021-10-25 03:40:27 -0700

    working on show question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,14 @@ function setNextQuestion(){
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if(answer.correct) {
+            button.dataser.correct = answer.correct
+        }
+    })
 }
 
 
@@ -138,7 +146,7 @@ const questions = [
 
 
 
-//generate.addEventListener("click", generateQuiz);
+
 
 
 
